0406721d6e5651e06513180409e2fffca2451eb7
Wickeddev123 <wickedempire111@outlook.com>
2020-03-22 21:29:39 +1030

    Cleaned up the mistakes in my english. Changes files:  Program.cs
    Signed-off-by: Wickeddev123 <wickedempire111@outlook.com>

--- LibCopy/Program.cs
+++ LibCopy/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using CommandLine;
 
-//Todo: If Verbose is set, output the total size of the files.
+//Todo: If Verbose is set, output the total size of the files. 
 
 // Exit Codes:
 // 0: Success 
@@ -19,9 +19,9 @@ namespace LibCopy
         {
             [Option('v', "Verbose", Required = false, HelpText = "Outputs current state of the application.", Default = false)]
             public bool Verbose  { get; set; }
-            [Option('f', "File Paths", Required = true, HelpText = "Gives the application the file paths for the files to copy.")]
+            [Option('f', "File Paths", Required = true, HelpText = "Location of the files the application needs to copy.")]
             public string[] FilesToCopy { get; set; }
-            [Option('d', "Directory to copy files into.", Required = true, HelpText = "Gives the application the place where the files will be copied into.")]
+            [Option('d', "Directory to copy files into.", Required = true, HelpText = "Filepath where the files will be copied into.")]
             public string Directory { get; set; }
         }
         
