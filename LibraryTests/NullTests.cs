d1ee62f8e08b6511d8fcabe59c60a92237e3d301
GHXX <GHXX@gmx.at>
2020-03-22 15:27:59 +0100

    more tests!

---
+++ LibraryTests/NullTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LibraryTests
+{
+    [TestClass]
+    public class NullTests
+    {
+        [TestMethod]
+        public void NullCheckFileExists()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => LibCopy.Utils.VerifyFile(null));
+        }
+
+        [TestMethod]
+        public void EmptyCheckFileExists()
+        {
+            Assert.ThrowsException<ArgumentException>(() => LibCopy.Utils.VerifyFile(""));
+        }
+
+        [TestMethod]
+        public void WhitespaceCheckFileExists()
+        {
+            Assert.ThrowsException<ArgumentException>(() => LibCopy.Utils.VerifyFile(" "));
+        }
+
+        [TestMethod]
+        public void NullCheckDirectoryExists()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => LibCopy.Utils.VerifyDirectory(null));
+        }
+
+        [TestMethod]
+        public void EmptyCheckDirectoryExists()
+        {
+            Assert.ThrowsException<ArgumentException>(() => LibCopy.Utils.VerifyDirectory(""));
+        }
+
+        [TestMethod]
+        public void WhitespaceCheckDirectoryExists()
+        {
+            Assert.ThrowsException<ArgumentException>(() => LibCopy.Utils.VerifyDirectory(" "));
+        }
+    }
+}
